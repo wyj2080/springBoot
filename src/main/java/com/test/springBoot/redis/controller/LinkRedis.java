1d897e5a48d782bd1c8e52178f3721dee082f948
wangyinjia <1603068044@qq.com>
2019-11-15 09:36:51 +0800

    redis，set/get

--- src/main/java/com/test/springBoot/redis/controller/LinkRedis.java
+++ src/main/java/com/test/springBoot/redis/controller/LinkRedis.java
@@ -1,8 +1,10 @@
 package com.test.springBoot.redis.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,15 +19,28 @@ import java.util.Map;
 @RequestMapping("/link/redis")
 public class LinkRedis {
 
+    @Value("${redis.servers}")
+    private String servers;
+
     @RequestMapping(value = "/send", method = RequestMethod.GET)
     public Map<String, Object> send() throws InterruptedException {
-
+        //连接 Redis 服务
+        Jedis jedis = new Jedis(servers);
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+        jedis.set("username","dapeng");
+        jedis.set("password","123456");
+        jedis.close();
         return new HashMap<>();
     }
 
     @RequestMapping(value = "/receive", method = RequestMethod.GET)
     public Map<String, Object> receive() throws InterruptedException {
-
+        Jedis jedis = new Jedis(servers);
+        System.out.println("username:" + jedis.get("username") + ",password:" + jedis.get("password"));
+        System.out.println(jedis.get("unknowKey"));
+        System.out.println(jedis.get("unknowKey") == null);
         return new HashMap<>();
     }
 
