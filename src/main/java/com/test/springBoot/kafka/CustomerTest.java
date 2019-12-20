1203522a1533ba33065153e396305d4c1f5d7bb2
wyj2080 <1603068044@qq.com>
2019-12-20 13:37:40 +0800

    kafka集群，分区

--- src/main/java/com/test/springBoot/kafka/CustomerTest.java
+++ src/main/java/com/test/springBoot/kafka/CustomerTest.java
@@ -1,42 +1,47 @@
-//package com.test.springBoot.kafka;
-//
-//import org.apache.kafka.clients.consumer.ConsumerRecord;
-//import org.apache.kafka.clients.consumer.ConsumerRecords;
-//import org.apache.kafka.clients.consumer.KafkaConsumer;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Arrays;
-//import java.util.Properties;
-//
-///**
-// * @Description: 消费者
-// * @Author: wangyinjia
-// * @Date: 2019/10/28
-// * @Version: 1.0
-// */
-//@Service
-//public class CustomerTest {
-//    public void receive() throws InterruptedException {
-//        Properties properties = new Properties();
-//        properties.put("bootstrap.servers", "45.32.66.42:9092");
-//        properties.put("group.id", "group-1");
-//        properties.put("enable.auto.commit", "true");
-//        properties.put("auto.commit.interval.ms", "1000");
-//        properties.put("auto.offset.reset", "earliest");
-//        properties.put("session.timeout.ms", "30000");
-//        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-//        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-//
-//        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
-//        kafkaConsumer.subscribe(Arrays.asList("jcftest","joker"));
-//        while (true) {
-//            ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
-//            for (ConsumerRecord<String, String> record : records) {
-//                System.out.printf("topic = %s, offset = %d, value = %s, partion = %s", record.topic(), record.offset(), record.value(), record.partition());
-//                System.out.println("====================================>");
-//            }
-//        }
-////        kafkaConsumer.close();
-//
-//    }
-//}
+package com.test.springBoot.kafka;
+
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @Description: 消费者
+ * @Author: wangyinjia
+ * @Date: 2019/10/28
+ * @Version: 1.0
+ */
+@Service
+public class CustomerTest {
+        //服务器
+    @Value("${kafka.servers}")
+    private String servers;
+    public void receive() throws InterruptedException {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", servers);
+        properties.put("group.id", "group-1");
+        properties.put("enable.auto.commit", "true");
+        properties.put("auto.commit.interval.ms", "1000");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("session.timeout.ms", "30000");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Arrays.asList("jcftest","joker","test"));
+        while (true) {
+            ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("topic = %s, offset = %d, value = %s, partion = %s", record.topic(), record.offset(), record.value(), record.partition());
+                System.out.println("====================================>");
+            }
+        }
+//        kafkaConsumer.close();
+
+    }
+}
--- src/main/java/com/test/springBoot/kafka/KafkaProducerTest.java
+++ src/main/java/com/test/springBoot/kafka/KafkaProducerTest.java
@@ -1,4 +1,4 @@
-/*
+
 package com.test.springBoot.kafka;
 
 import java.util.Properties;
@@ -6,14 +6,38 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerTest {
 
+    //服务器
+    @Value("${kafka.servers}")
+    private String servers;
+
     public static void main(String[] args){
+//        Properties props = new Properties();
+//        props.put("bootstrap.servers", servers);
+//        props.put("acks", "all");
+//        props.put("retries", 0);
+//        props.put("batch.size", 16384);
+//        props.put("linger.ms", 1);
+//        props.put("buffer.memory", 33554432);
+//        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+//
+//        Producer<String, String> producer = new KafkaProducer<>(props);
+//        for(int i = 0; i < 100; i++)
+//            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+//
+//        producer.close();
+
+    }
+
+    public void send(String topic, String msg){
         Properties props = new Properties();
-        props.put("bootstrap.servers", "45.32.71.100:9092");
+        props.put("bootstrap.servers", servers);
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("batch.size", 16384);
@@ -23,11 +47,9 @@ public class KafkaProducerTest {
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for(int i = 0; i < 100; i++)
-            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), Integer.toString(i)));
+        producer.send(new ProducerRecord<>(topic, msg));
 
         producer.close();
-
     }
 }
-*/
+
