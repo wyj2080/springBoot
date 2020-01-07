e4d9db9aa9835f6a7288e58efdf956f3443b3deb
wyj2080 <1603068044@qq.com>
2020-01-07 10:25:56 +0800

    字符串过长处理成"..."

---
+++ src/main/resources/static/js/stringTooLong.js
@@ -0,0 +1,28 @@
+/**
+ * 字符串过长处理
+ * @param str
+ * @param len
+ * @returns {string|*}
+ */
+function cutString(str, len) {
+    if(str.length*2 <= len) {
+        return str;
+    }
+    var strlen = 0;
+    var s = "";
+    for(var i = 0;i < str.length; i++) {
+        s = s + str.charAt(i);
+        if (str.charCodeAt(i) > 128) {
+            strlen = strlen + 2;
+            if(strlen >= len){
+                return s.substring(0,s.length-1) + "...";
+            }
+        } else {
+            strlen = strlen + 1;
+            if(strlen >= len){
+                return s.substring(0,s.length-2) + "...";
+            }
+        }
+    }
+    return s;
+}
