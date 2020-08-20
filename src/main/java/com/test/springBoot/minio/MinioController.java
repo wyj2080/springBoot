80ac30a33d0f6f1bffedb2178feb405a3894a05f
1603068044@qq.com <1603068044@qq.com>
2020-08-20 13:03:29 +0800

    minio bucket

--- src/main/java/com/test/springBoot/minio/MinioController.java
+++ src/main/java/com/test/springBoot/minio/MinioController.java
@@ -3,9 +3,12 @@ package com.test.springBoot.minio;
 import io.minio.MinioClient;
 import io.minio.errors.InvalidEndpointException;
 import io.minio.errors.InvalidPortException;
+import io.minio.messages.Bucket;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 /**
  * @Description: minio
@@ -16,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/minio")
 public class MinioController {
+    String endPoint = "http://ip:9000";
+    String accessKey = "账户";
+    String secretKey = "密码";
 
     @RequestMapping(value = "/download", method = RequestMethod.GET)
     public void download(){
@@ -24,7 +30,7 @@ public class MinioController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.GET)
     public void upload() throws Exception {
-        MinioClient minioClient = new MinioClient("http://ip:9000", "账户", "密码");
+        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
         // 检查存储桶是否已经存在
         boolean isExist = minioClient.bucketExists("test");
         if(isExist) {
@@ -38,4 +44,16 @@ public class MinioController {
         minioClient.putObject("test","tt.xlsx", "d://test.xlsx");
 
     }
+
+    @RequestMapping(value = "/bucket", method = RequestMethod.GET)
+    public void bucket() throws Exception {
+        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
+        if(!minioClient.bucketExists("store")){
+            minioClient.makeBucket("store");
+        }
+        List<Bucket> bucketList = minioClient.listBuckets();
+        bucketList.forEach(t -> System.out.println(t.toString()));
+
+    }
+
 }
