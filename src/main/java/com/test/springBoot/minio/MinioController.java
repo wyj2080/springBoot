505b4b75586f18dd070eab6155ba91804fb82321
wangyinjia <1603068044@qq.com>
2020-08-20 22:12:56 +0800

    minio桶操作，对象操作

--- src/main/java/com/test/springBoot/minio/MinioController.java
+++ src/main/java/com/test/springBoot/minio/MinioController.java
@@ -1,13 +1,22 @@
 package com.test.springBoot.minio;
 
 import io.minio.MinioClient;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
+import io.minio.Result;
+import io.minio.errors.*;
 import io.minio.messages.Bucket;
+import io.minio.messages.Item;
+import io.minio.messages.Upload;
+import io.minio.policy.PolicyType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.ZoneId;
 import java.util.List;
 
 /**
@@ -19,9 +28,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/minio")
 public class MinioController {
-    String endPoint = "http://ip:9000";
-    String accessKey = "账户";
-    String secretKey = "密码";
+    String endPoint = "http://39.101.180.162:9000";
+    String accessKey = "admin";
+    String secretKey = "mn123456";
 
     @RequestMapping(value = "/download", method = RequestMethod.GET)
     public void download(){
@@ -45,14 +54,52 @@ public class MinioController {
 
     }
 
+    /**
+     * 桶
+     */
     @RequestMapping(value = "/bucket", method = RequestMethod.GET)
     public void bucket() throws Exception {
         MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
         if(!minioClient.bucketExists("store")){
+            //创建桶
             minioClient.makeBucket("store");
         }
+        //桶list
         List<Bucket> bucketList = minioClient.listBuckets();
-        bucketList.forEach(t -> System.out.println(t.toString()));
+        //删除桶（需空）
+        minioClient.makeBucket("delete");
+        minioClient.removeBucket("delete");
+        //桶内对象list
+        Iterable<Result<Item>> objectList = minioClient.listObjects("store", "文件名前缀搜索");
+        objectList.forEach(t -> {
+            try {
+                System.out.println("最后修改日期：" + t.get().lastModified().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()+
+                        ", 名称："+t.get().objectName()+", 大小："+ t.get().objectSize());
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        });
+        //部分上传的文件
+        Iterable<Result<Upload>> errorList = minioClient.listIncompleteUploads("store");
+        errorList.forEach(t -> {
+            try {
+                System.out.println("名称："+t.get().objectName());
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        });
+        //桶策略
+        minioClient.getBucketPolicy("store");
+        minioClient.setBucketPolicy("store", "", PolicyType.NONE);
+    }
+
+    /**
+     * 对象操作
+     */
+    @RequestMapping(value = "/obj", method = RequestMethod.GET)
+    public void obj() throws Exception {
+        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
+        InputStream inputStream = minioClient.getObject("store", "vue.txt");
 
     }
 
