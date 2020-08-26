ecd9117116c7b2fb098e899ca4f553887b517317
wangyinjia <1603068044@qq.com>
2020-08-26 21:24:12 +0800

    minio文件上传，删除，复制

--- src/main/java/com/test/springBoot/minio/MinioController.java
+++ src/main/java/com/test/springBoot/minio/MinioController.java
@@ -1,6 +1,7 @@
 package com.test.springBoot.minio;
 
 import io.minio.MinioClient;
+import io.minio.ObjectStat;
 import io.minio.Result;
 import io.minio.errors.*;
 import io.minio.messages.Bucket;
@@ -12,14 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.xmlpull.v1.XmlPullParserException;
 
+import javax.annotation.PostConstruct;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.time.ZoneId;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,6 +38,12 @@ public class MinioController {
     String endPoint = "http://ip:9000";
     String accessKey = "账号";
     String secretKey = "密码";
+    MinioClient minioClient;
+
+    @PostConstruct
+    public void init() throws Exception {
+        minioClient = new MinioClient(endPoint, accessKey, secretKey);
+    }
 
     @RequestMapping(value = "/download", method = RequestMethod.GET)
     public void download(){
@@ -42,7 +52,6 @@ public class MinioController {
 
     @RequestMapping(value = "/upload", method = RequestMethod.GET)
     public void upload() throws Exception {
-        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
         // 检查存储桶是否已经存在
         boolean isExist = minioClient.bucketExists("test");
         if(isExist) {
@@ -62,7 +71,6 @@ public class MinioController {
      */
     @RequestMapping(value = "/bucket", method = RequestMethod.GET)
     public void bucket() throws Exception {
-        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
         if(!minioClient.bucketExists("store")){
             //创建桶
             minioClient.makeBucket("store");
@@ -101,21 +109,30 @@ public class MinioController {
      */
     @RequestMapping(value = "/obj", method = RequestMethod.GET)
     public void obj() throws Exception {
-        MinioClient minioClient = new MinioClient(endPoint, accessKey, secretKey);
         InputStream inputStream = minioClient.getObject("store", "vue.txt");
         printInputStream(inputStream);
         // 关闭流，此处为示例，流关闭最好放在finally块。
         inputStream.close();
-        //检查对象存在，不存在会抛异常
-        minioClient.statObject("store", "vue.txt");
+        //检查对象存在，不存在会抛异常(对象信息)
+        ObjectStat objectStat = minioClient.statObject("store", "vue.txt");
         //直接下载到本地
         minioClient.getObject("store", "vue.txt", "d://vue.txt");
-        //生成256位AES key。
-        KeyGenerator symKeyGenerator = KeyGenerator.getInstance("AES");
-        symKeyGenerator.init(256);
-        SecretKey symKey = symKeyGenerator.generateKey();
-        InputStream skeyStream = minioClient.getObject("store", "vue.txt", symKey);
-        printInputStream(skeyStream);
+
+        //流上传成文件
+        StringBuilder builder = new StringBuilder();
+        builder.append("文字");
+        InputStream strStream = new ByteArrayInputStream(builder.toString().getBytes());
+        minioClient.putObject("store", "test.txt",strStream, strStream.available(), "application/octet-stream");
+        //本地文件上传
+        minioClient.putObject("store", "test.xlsx","d://test.xlsx");
+        //复制对象
+        minioClient.copyObject("store",  "test.txt", "store", "test2.txt");
+        //删除对象
+//        minioClient.removeObject("store", "test.txt");
+//        minioClient.removeObject("store", "test.xlsx");
+//        minioClient.removeObject("store", "test3.xlsx");
+        List<String> fileNames = Arrays.asList("test.txt", "test.xlsx");
+        minioClient.removeObject("store", fileNames);//还没成功
     }
 
     /**
@@ -128,6 +145,26 @@ public class MinioController {
         while ((bytesRead = inputStream.read(buf, 0, buf.length)) >= 0) {
             System.out.println(new String(buf, 0, bytesRead, StandardCharsets.UTF_8));
         }
+    }
+
+    /**
+     * 加密对象
+     */
+    @RequestMapping(value = "/secret", method = RequestMethod.GET)
+    public void secret() throws Exception {
+        //生成256位AES key.
+        KeyGenerator symKeyGenerator = KeyGenerator.getInstance("AES");
+        symKeyGenerator.init(256);
+        SecretKey symKey = symKeyGenerator.generateKey();
+        //流上传成文件
+        StringBuilder builder = new StringBuilder();
+        builder.append("文字");
+        InputStream strStream = new ByteArrayInputStream(builder.toString().getBytes());
+        minioClient.putObject("store", "test.txt",strStream, strStream.available(), "application/octet-stream", symKey);
+
+        //生成256位AES key。(未成功)
+        InputStream skeyStream = minioClient.getObject("store", "vue.txt", symKey);
+        printInputStream(skeyStream);
     }
 
 }
