10bf8d2e25cddd5ee0c57c56d8fa7c05e817a6e3
wangyinjia <1603068044@qq.com>
2020-12-10 21:33:21 +0800

    jetcache初始化

--- src/main/java/com/test/springBoot/Application.java
+++ src/main/java/com/test/springBoot/Application.java
@@ -1,18 +1,17 @@
 package com.test.springBoot;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan({"com.test.springBoot.*.mapper"})
+@EnableMethodCache(basePackages = "com.test.springBoot")
+@EnableCreateCacheAnnotation
 public class Application extends SpringBootServletInitializer {
 	/**
 	 * 实现SpringBootServletInitializer
