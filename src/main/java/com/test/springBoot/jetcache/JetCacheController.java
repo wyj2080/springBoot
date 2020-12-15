d116f5490e5ac1835581313f0a6015cafefda2a2
wangyinjia <1603068044@qq.com>
2020-12-15 21:46:01 +0800

    jetcache增删改，方法缓存

--- src/main/java/com/test/springBoot/jetcache/JetCacheController.java
+++ src/main/java/com/test/springBoot/jetcache/JetCacheController.java
@@ -2,10 +2,12 @@ package com.test.springBoot.jetcache;
 
 import com.alicp.jetcache.Cache;
 import com.alicp.jetcache.anno.CacheType;
+import com.alicp.jetcache.anno.Cached;
 import com.alicp.jetcache.anno.CreateCache;
 import com.test.springBoot.java8.UserDO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,8 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/jetcache")
 public class JetCacheController {
-    //DO类需要实现序列化,expire:过期时间
-    @CreateCache(name = "JetCacheController.userCache", expire = 100, cacheType = CacheType.BOTH, localLimit = 50)
+    //DO类需要实现序列化
+    // expire:过期时间
+    // localLimit内存中的数量
+    // cacheType默认远程
+    @CreateCache(name = "JetCacheController.userCache", expire = 50, cacheType = CacheType.BOTH, localLimit = 50)
     private Cache<Long, UserDO> userCache;
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
@@ -26,14 +31,26 @@ public class JetCacheController {
         UserDO userDO = new UserDO();
         userDO.setId(1001L);
         userDO.setName("小五");
-        userDO.setAge(18);
+        userDO.setAge(19);
+        //存
         userCache.put(userDO.getId(), userDO);
+        //取
+        userCache.get(1001L);
+        //删除
+//        userCache.remove(1001L);
         System.out.println(userCache.get(1001L).toString());
     }
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     public void get() throws Exception {
         System.out.println(userCache.get(1001L).toString());
+    }
+
+    @RequestMapping(value = "/bean", method = RequestMethod.GET)
+    @ResponseBody
+    @Cached(name="JetCacheController.getBean", expire = 30)
+    public UserDO getBean() throws Exception {
+        return userCache.get(1001L);
     }
 
 
