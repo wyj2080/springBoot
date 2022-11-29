329abde127be43876d47f80298baeb826e4770b2
wangyinjia <1603068044@qq.com>
2022-11-29 21:27:15 +0800

    多数据源分页bug

--- src/main/java/com/test/springBoot/mybatisPlus/MyBatisPlusConfig.java
+++ src/main/java/com/test/springBoot/mybatisPlus/MyBatisPlusConfig.java
@@ -1,7 +1,9 @@
 package com.test.springBoot.mybatisPlus;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -22,6 +24,8 @@ public class MyBatisPlusConfig {
     public MybatisPlusInterceptor optimisticLockerInnerInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //多数据源，分页失效解决
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
 
